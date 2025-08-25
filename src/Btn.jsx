28d184148051eebb39f4b04f6625c4c82cba7e62
Divinejay-001 <divinegiftjohnson21@gmail.com>
2025-08-24 22:53:40 -0700

    send btn correction

--- src/Btn.jsx
+++ src/Btn.jsx
@@ -5,7 +5,7 @@ const Btn = ({text}) => {
     <div className='flex justify-center items-center md:justify-start'>
     <button value='send' type='submit' className={`submitBtn bg-gradient-to-r from-purple-500 to-blue-600 text-white 
 transform transition-transform duration-300 
-hover:scale-105  px-24 py-2 rounded-md`}>
+hover:scale-105 px-8  md:px-24 py-2 rounded-md`}>
        {text}
        
    </button>
