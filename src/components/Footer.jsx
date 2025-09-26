12d4032958297b95fce2d9225cf043dd9c751f07
Divinejay-001 <divinegiftjohnson21@gmail.com>
2025-09-25 22:04:43 -0700

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,6 @@
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
 export default function Footer() {
   return (
     <footer className="py-8 bg-gray-950 border-t border-purple-500/20 text-center text-gray-400">
@@ -5,9 +8,15 @@ export default function Footer() {
         © {new Date().getFullYear()} <span className="text-purple-400 font-bold">Divinora</span>. All rights reserved.
       </p>
       <div className="flex justify-center gap-6">
-        <a href="#" className="hover:text-purple-400 transition">LinkedIn</a>
-        <a href="#" className="hover:text-purple-400 transition">GitHub</a>
-        <a href="#" className="hover:text-purple-400 transition">Twitter</a>
+        <a href="https://www.linkedin.com/in/divinegift-johnson-739977324/" className="hover:text-purple-400 border px-2 py-2 rounded transition">
+          <FaLinkedin size={20} />
+        </a>
+        <a href="https://github.com/Divinejay-001" className="hover:text-purple-400 border px-2 py-2 rounded transition">
+          <FaGithub size={20} />
+        </a>
+        <a href="https://x.com/DivinoraHQ" className="hover:text-purple-400 border px-2 py-2 rounded transition">
+          <FaTwitter size={20} />
+        </a>
       </div>
     </footer>
   );
